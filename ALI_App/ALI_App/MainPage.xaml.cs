6d655ab4e83963d1e3aa963078ee99d2c5d1f2b4
Mohamad Tarek Alabbar <Alabbar.tarek@gmail.com>
2018-12-16 01:13:40 +0000

    Starting

---
+++ ALI_App/ALI_App/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ALI_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void byNameSearchButton_Clicked(object sender, EventArgs e)
+        {
+            string name = byNameSearchEntry.Text;
+            //DisplayAlert("Hello", "This is the Search by NAME button: " + name, "Ok");
+            Navigation.PushAsync(new DataListView());
+        }
+
+        private void byPlaceSearchButton_Clicked(object sender, EventArgs e)
+        {
+            string place = byPlaceSearchEntry.Text;
+            DisplayAlert("Hello", "This is the Search by PLACE button: " + place, "Ok");
+        }
+    }
+}
