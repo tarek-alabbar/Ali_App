93e5b0afcd23ed2e3ed84d7d504f1fffdef745df
Mohamad Tarek Alabbar <Alabbar.tarek@gmail.com>
2018-12-22 22:55:41 +0000

    NOTWORKING azure data handler

---
+++ ALI_App/ALI_App/AzureDataService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALI_App
+{
+    public class AzureDataService
+    {
+        public MobileServiceClient MobileService { get; set; }
+        IMobileServiceSyncTable coffeeTable;
+
+        public async Task Initialize()
+        {
+            //Create our client
+            MobileService = new MobileServiceClient("http://aliphonebookapp.azurewebsites.net/");
+
+            const string path = "syncstore.db";
+            //setup our local sqlite store and intialize our table
+            var store = new MobileServiceSQLiteStore(path);
+            store.DefineTable();
+            await MobileService.SyncContext.InitializeAsync(store, new MobileServiceSyncHandler());
+
+            //Get our sync table that will call out to azure
+            coffeeTable = MobileService.GetSyncTable();
+        }
+
+        public async IEnumerable GetData()
+        {
+
+        }
+
+        public async Task AddData(bool madeAtHome)
+        {
+
+        }
+
+        public async Task SyncData()
+        {
+
+        }
+    }
+}
---
+++ ALI_App/ALI_App/ali_phonebook_data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALI_App
+{
+    public class ali_phonebook_data
+    {
+        [Newtonsoft.Json.JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.Version]
+        public string AzureVersion { get; set; }
+
+        public string name { get; set; }
+        public string city { get; set; }
+        public string place { get; set; }
+        public string job { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public string sons { get; set; }
+
+    }
+}
